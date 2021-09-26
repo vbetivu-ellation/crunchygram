e8741380795b3f2ecc45e89e07cd26e6161ac2b3
Vladislav <vladislav.betivu@ellation.com>
2021-09-26 17:37:14 +0300

    Add more eslint rules

--- src/components/common/Text/Text.js
+++ src/components/common/Text/Text.js
@@ -4,22 +4,16 @@ import PropTypes from "prop-types";
 
 import styles from "./Text.module.css";
 
-const Text = ({ children, className, size, weight }) => {
-  return (
-    <p
-      className={classNames(
-        styles.text,
-        styles[weight],
-        styles[size],
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
-};
+const Text = ({ as: As, children, className, size, weight }) => (
+  <As
+    className={classNames(styles.text, styles[weight], styles[size], className)}
+  >
+    {children}
+  </As>
+);
 
 Text.propTypes = {
+  as: PropTypes.oneOf(["p", "span", "small"]).isRequired,
   children: PropTypes.node.isRequired,
   size: PropTypes.oneOf(["s", "m", "l", "xl"]).isRequired,
   className: PropTypes.string,
