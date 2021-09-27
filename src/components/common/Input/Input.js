7cb01189b1459c5c431e3668bd1f4d70079733a1
Vladislav <vladislav.betivu@ellation.com>
2021-09-27 20:24:05 +0300

    Create Input component

---
+++ src/components/common/Input/Input.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+import styles from "./Input.module.css";
+import Text from "../Text";
+
+const Input = ({
+  className,
+  onChange,
+  value,
+  isDisabled,
+  label,
+  placeholder,
+}) => (
+  <label className={styles.label}>
+    <Text as="small" size="s" className={styles["label-text"]}>
+      {label}
+    </Text>
+    <input
+      placeholder={placeholder}
+      value={value}
+      className={classNames(styles.input, className)}
+      disabled={isDisabled}
+      onChange={onChange}
+    />
+  </label>
+);
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+
+export default Input;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,6 @@
 import Text from "./Text";
 import Heading from "./Heading";
 import Button from "./Button";
+import Input from "./Input";
 
-export { Text, Heading, Button };
+export { Text, Heading, Button, Input };
