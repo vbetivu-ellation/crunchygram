070180488f6c4dfa3857bac7200813446b51dd2f
Diana Arsenii <diana.arsenii@crunchyroll.com>
2021-09-28 12:16:15 +0300

    Export Comment and Search Svgs

--- src/components/common/Svg/index.js
+++ src/components/common/Svg/index.js
@@ -1,4 +1,6 @@
 import LikeFilledSvg from "./components/LikeFilledSvg";
 import LikeSvg from "./components/LikeSvg";
+import CommentSvg from "./components/CommentSvg";
+import SearchSvg from "./components/SearchSvg";
 
-export { LikeFilledSvg, LikeSvg };
+export { LikeFilledSvg, LikeSvg, CommentSvg, SearchSvg };
