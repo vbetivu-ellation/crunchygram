11ded8e1fdf2bf9821a6df59fab95cf316c70480
Vladislav <vladislav.betivu@ellation.com>
2021-09-30 17:13:54 +0300

    Add button to textarea

---
+++ src/components/common/Svg/components/SendSvg.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import BaseSvg from "./BaseSvg";
+
+const ForwardSvg = () => (
+  <BaseSvg viewBox="0 0 24 24">
+    <path d="M5 11h11.17l-4.882-4.882a.997.997 0 0 1 0-1.41l.002-.003.003-.003a1 1 0 0 1 1.412.003l6.588 6.588a1 1 0 0 1 0 1.414l-6.588 6.588a.997.997 0 0 1-1.41-1.41L16.17 13H5a1 1 0 0 1 0-2z" />
+  </BaseSvg>
+);
+
+export default ForwardSvg;
