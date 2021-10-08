0d45ab49bb4579bb7ec5938354fc4b51e3608d9e
ElenaTimbur <elena.timbur@crunchyroll.com>
2021-10-08 18:29:13 +0300

    export SVG from common

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,5 @@
 import Text from "./Text";
 import LikeSvg from "./LikeSvg";
-import SearchSVG from "./Components/SearchSVG";
+import { SearchSVG } from "./SVG";
 
 export { Text, LikeSvg, SearchSVG };
