7785e394596d339349716e58697ae8f064901e0a
Пользователь Asus <arina.cimpac@crunchyroll.com>
2021-10-08 18:56:45 +0300

    restored App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { Text , Input} from "./components/common";
-
+import { Text } from "./components/common";
+import StoreProvider from "./store";
+import Counter from "./components/Counter";
 
 function App() {
-  const [value, setValue] = useState("");
-   const handleChange=(event)=>
-    setValue(event.target.value);
-  
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <Text as="p" size="xl" weight="bold">
-          Edit <code>src/App.js</code> and save to reload.
-        </Text>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Input value={value} handleChange={handleChange}/>
-
-
-    </div>
+    <StoreProvider>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <Counter />
+          <Text as="p" size="xl" weight="bold">
+            Edit <code>src/App.js</code> and save to reload.
+          </Text>
+          <a
+            className={styles.link}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </StoreProvider>
   );
 }
 
