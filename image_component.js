edc1c0b41f901cbff4ce13c1437d6cf2a0e36b42
loredanaspivac <92035977+loredanaspivac@users.noreply.github.com>
2021-10-08 17:20:23 +0300

    Add files via upload

---
+++ image_component.js
@@ -0,0 +1,8 @@
+
+
+<link rel="stylesheet" href="stylesheet.css"></link>
+
+const img = document.createElement("img");
+img.src = "https://beta.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/6e5065403981315750fa9bd117365cb7.jpg";
+document.body.appendChild(img);
+
