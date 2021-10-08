3635d92fa687f25236db9bfc7f07780b208f78aa
Panfil Alexandrina <alexandrina.panfil@crunchyroll.com>
2021-10-08 18:08:02 +0300

    24px

--- src/components/common/CommentSvg/CommentSvg.js
+++ src/components/common/CommentSvg/CommentSvg.js
@@ -3,8 +3,8 @@ import React from "react";
 const CommentSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="25px"
-    height="25px"
+    width="24px"
+    height="24px"
     viewBox="0 0 24 24"
     role="img"
   >
