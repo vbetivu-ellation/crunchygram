59b42bddb6796c7d698decb0f3b04fe0bbf44987
rxcva <vlada.bulai20@gmail.com>
2021-10-09 00:07:08 +0300

    Add responsive heading components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import styles from "./App.module.css";
 import { Text } from "./components/common";
 import StoreProvider from "./store";
 import Counter from "./components/Counter";
+import { HeadingL } from "./components/HeadingL/HeadingL.jsx"
+import { HeadingXL } from './components/HeadingXL/HeadingXL.jsx'
 
 
 function App() {
@@ -10,6 +12,21 @@ function App() {
     <StoreProvider>
       <div className={styles.app}>
         <header className={styles.header}>
+
+          <HeadingL
+            bgColor={'blue'}
+            title="Title example 0001 Heading L"
+            content="Text example"
+            lineColor="red"
+          />
+
+          <HeadingXL
+            bgColor={'orange'}
+            title="Title example 0001 Heading XL"
+            content="Text example"
+            lineColor="green"
+          />
+
           <img src={logo} className={styles.logo} alt="logo" />
           <Counter />
           <Text as="p" size="xl" weight="bold">
