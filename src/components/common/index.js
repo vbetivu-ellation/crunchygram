f0357f7593da3099b69394f5c0669a8dd7354259
MihaelaCatan04 <66206241+MihaelaCatan04@users.noreply.github.com>
2021-10-09 14:44:36 +0300

    LogoutSvg (#54)

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,8 @@
 import Text from "./Text";
 import CommentSvg from "./CommentSvg";
 import LikeSvg from "./LikeSvg";
+import LogoutSvg from "./LogoutSvg";
 import { SearchSVG } from "./SVG";
 
-export { Text, LikeSvg, SearchSVG, CommentSvg };
+export { Text, LikeSvg, CommentSvg, LogoutSvg, SearchSVG };
+
