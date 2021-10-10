b20553260beeb3bbfb5f2dae53ac3d0a1f8351c4
Bostan <sanda.bostan@crunchyroll.com>
2021-10-10 16:20:00 +0300

    fix conflict

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,7 @@
 import Text from "./Text";
+import CommentSvg from "./CommentSvg";
+import LikeSvg from "./LikeSvg";
+import { SearchSVG } from "./SVG";
 import FilledLikeSvg from "./FilledLikeSvg";
-export { Text, FilledLikeSvg };
+
+export { Text, LikeSvg, SearchSVG, CommentSvg, FilledLikeSvg };
