e4637284e290db0d8e77dfd8d40f633c435d320f
rxcva <vlada.bulai20@gmail.com>
2021-10-11 18:15:25 +0300

    Edit component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,6 @@ import Text from "./Text";
 import CommentSvg from "./CommentSvg";
 import LikeSvg from "./LikeSvg";
 import { SearchSVG } from "./SVG";
+import Heading from "./Heading"
 
-export { Text, LikeSvg, SearchSVG, CommentSvg };
+export { Text, LikeSvg, SearchSVG, CommentSvg, Heading };
