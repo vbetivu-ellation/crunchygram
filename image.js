05d5d09039a1104eca22927bf1d155487be0d623
loredanaspivac <92035977+loredanaspivac@users.noreply.github.com>
2021-10-11 15:42:44 +0300

    Add files via upload

---
+++ image.js
@@ -0,0 +1,51 @@
+
+
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-js';
+
+const styles = image.module.css({
+  
+  pic1: {
+    width: 100,
+    height: 100,
+  },
+ 
+});
+ 
+const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+    
+      <Image
+        style={styles.pic1}
+        source={{
+          uri: 'https://beta.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/6e5065403981315750fa9bd117365cb7.jpg',
+        }}
+      />
+    </View>
+  );
+}
+
+export default DisplayAnImage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
