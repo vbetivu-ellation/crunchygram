064c820198293e0ad39e664358e467e58fea4830
sakura-felicia <78251874+sakura-felicia@users.noreply.github.com>
2021-10-11 10:24:21 +0100

    Logo added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-import { Text } from "./components/common";
-import StoreProvider from "./store";
-import Counter from "./components/Counter";
+
+import Logo from "./components/common/Logo"
+
 
 function App() {
   return (
-    <StoreProvider>
-      <div className={styles.app}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <Counter />
-          <Text as="p" size="xl" weight="bold">
-            Edit <code>src/App.js</code> and save to reload.
-          </Text>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </StoreProvider>
+    <div>
+      <Logo/>
+    </div>
   );
 }
 
