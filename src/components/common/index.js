573e2a05537337da215abe78853deda3b8ecb186
Пользователь Asus <arina.cimpac@crunchyroll.com>
2021-10-13 16:45:01 +0300

    final?? change

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,6 +2,6 @@ import Text from "./Text";
 import CommentSvg from "./CommentSvg";
 import LikeSvg from "./LikeSvg";
 import { SearchSVG } from "./SVG";
-import { Input } from "./Input"
+import Input from "./Input"
 
 export { Text, LikeSvg, SearchSVG, CommentSvg, Input };
