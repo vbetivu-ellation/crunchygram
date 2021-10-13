9425028ab5b0d4fd99c8c355772b447b24ac42e1
MihaelaCatan04 <mihaelacatan7@gmail.com>
2021-10-13 16:56:19 +0300

    Added EmptyPosts component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,15 +1,16 @@
 import Text from "./Text";
-import SpinnerSvg from "./SpinnerSvg";
+import SpinnerSvg  from "./SpinnerSvg";
 import CommentSvg from "./CommentSvg";
 import LikeSvg from "./LikeSvg";
 import LogoutSvg from "./LogoutSvg";
 import { SearchSVG } from "./SVG";
 import Avatar from "./Avatar";
 import Image from "./Image";
-import Heading from "./Heading" 
+import Heading from "./Heading";
 import FilledLikeSvg from "./FilledLikeSvg";
+import EmptyPosts from "./EmptyPosts";
 
- export {
+export {
   Text,
   LikeSvg,
   CommentSvg,
@@ -20,5 +21,5 @@ import FilledLikeSvg from "./FilledLikeSvg";
   SpinnerSvg,
   Heading,
   FilledLikeSvg,
- };
- 
+  EmptyPosts,
+};
