0c1c8fd13c79f015f6174381e50ecb5b58bcc54d
Panfil Alexandrina <alexandrina.panfil@crunchyroll.com>
2021-10-15 18:26:01 +0300

    profile post

--- src/components/common/ProfilePostgrid/ProfilePostGrid.js
+++ src/components/common/ProfilePostgrid/ProfilePostGrid.js
@@ -2,23 +2,33 @@ import React from "react";
 import ProfilePost from "../ProfilePost";
 import styles from "./ProfilePostGrid.module.css";
 
+const posts = [
+  {
+    src: "https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg",
+    likesCount: 20,
+    commentsCount: 30,
+  },
+  {
+    src: "https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg",
+    likesCount: 1,
+    commentsCount: 4,
+  },
+  {
+    src: "https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg",
+    likesCount: 15,
+    commentsCount: 12,
+  },
+];
+
 const ProfilePostGrid = () => (
   <div className={styles.grid}>
-    <ProfilePost
-      src="https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg"
-      likesCount={20}
-      commentsCount={30}
-    />
-    <ProfilePost
-      src="https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg"
-      likesCount={20}
-      commentsCount={30}
-    />
-    <ProfilePost
-      src="https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg"
-      likesCount={20}
-      commentsCount={30}
-    />
+    {posts.map((post) => (
+      <ProfilePost
+        src={post.src}
+        likesCount={post.likesCount}
+        commentsCount={post.likesCount}
+      />
+    ))}
   </div>
 );
 
