498cd40550263930434e3dc85cdf44cb5829e0dc
Anyaaaaaaaaaa <samohvalov.ana14@gmail.com>
2021-10-15 18:00:30 +0300

    buton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import StoreProvider from "./store";
-
 const App = () => (
   <StoreProvider>
     <Router>
