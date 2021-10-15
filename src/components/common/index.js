d1ec3a75c845fb2c9dd9c534ddc76f4e6ccce2b3
Anyaaaaaaaaaa <samohvalov.ana14@gmail.com>
2021-10-15 18:20:52 +0300

    schimbări

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,11 +1,11 @@
 import Text from "./Text";
-import SpinnerSvg  from "./SpinnerSvg";
+import SpinnerSvg from "./SpinnerSvg";
 import CommentSvg from "./CommentSvg";
 import LikeSvg from "./LikeSvg";
 
 import LogoutSvg from "./LogoutSvg";
 import { SearchSVG } from "./SVG";
-import Input from "./Input"
+import Input from "./Input";
 
 import Avatar from "./Avatar";
 import Image from "./Image";
@@ -17,7 +17,6 @@ import Button from "./Button";
 import EmptyPosts from "./EmptyPosts";
 import Searchbutton from "../Searchbutton";
 
-
 export {
   Text,
   LikeSvg,
@@ -28,12 +27,9 @@ export {
   Image,
   Searchbutton,
   FilledLikeSvg,
-
   Button,
-};
-
   Heading,
   SpinnerSvg,
   EmptyPosts,
+  Input,
 };
-
